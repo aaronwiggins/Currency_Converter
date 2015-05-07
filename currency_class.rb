0c8417e4d9b54333768b77857042591486866c0b
Aaron Wiggins <AaronWiggins@Aarons-MacBook-Pro.local>
2015-05-07 17:29:24 -0400

    currency objects are equal to one another if they have the same amount and currency code They are not equal if they have a different amount or currency

--- currency_class.rb
+++ currency_class.rb
@@ -2,9 +2,15 @@
 # (a real-world thing you can point to),
 # United States Dollars
 class Currency #name == currency code
+  attr_reader :name, :amount
   def initialize(name, amount=1)
     @name = name
     @amount = amount
   end
 
+  def ==(other)
+    @name == other.name && @amount == other.amount
+  end
 end
+
+# check if @name && @amount == @name && @amount
--- currency_program.rb
+++ currency_program.rb
@@ -1,5 +1,8 @@
 require "./currency_class.rb"
 require "./converter_class.rb"
 
-usd = Currency.new("USD")
-p usd
+usd_a = Currency.new("USD", 50)
+usd_b = Currency.new("USD", 50)
+p usd_a.amount
+p usd_b.amount
+p usd_a == usd_b
