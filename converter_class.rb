df8e7c49fb0591bcd465617fcde6a8175f08f56f
Aaron Wiggins <apw7177@gmail.com>
2015-05-08 14:13:57 -0400

    convert USD to EUROS

--- converter_class.rb
+++ converter_class.rb
@@ -3,4 +3,13 @@ class Converter
   def initialize(conversion_rates)
     @conversion_rates = conversion_rates
   end
+
+  def convert(currency, code)
+    if currency.name == code
+      Currency.new(code, currency.amount)
+    else
+      Currency.new(code, currency.amount * conversion_rates[code])
+    end
+
+  end
 end
--- currency_program.rb
+++ currency_program.rb
@@ -14,4 +14,13 @@ usd_to_euro = Converter.new({USD: 1.0, EUR: 0.74})
 # p currency_three
 # p currency_three.name
 # p currency_three + currency_one
-p usd_to_euro
+# p usd_to_euro
+# p currency_one
+#
+# p currency_one.class
+# p currency_one.name.class
+p currency_one
+p usd_to_euro.convert(currency_one, :EUR)
+
+#(Currency.new(1, :USD), :USD)
+# usd_to_euro.convert
