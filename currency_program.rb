764312b0d3227e845eb648383a2a86edc816a9e9
Aaron Wiggins <apw7177@gmail.com>
2015-05-09 17:30:09 -0400

    initial currency trader class working. need to allow flexible conversions At the moment the methods have fixed codes

--- currency_program.rb
+++ currency_program.rb
@@ -2,13 +2,13 @@ require "./currency_class.rb"
 require "./converter_class.rb"
 require "./currency_trader_class.rb"
 
-currency_one = Currency.new(:USD, 50.00)
+currency_one = Currency.new(:USD, 100.00)
 currency_two = Currency.new(:EUR, 1.00)
 currency_three = Currency.new(:JPY, 1.00)
 currency_four = Currency.new("$ 7.00")
 currency_conversions = Converter.new({USD: 1.0, EUR: 0.74, JPY: 120.0})
-trade = Trader.new(currency_one)#, currency_conversions,
-#currency_conversions)
+trade = Trader.new(currency_one, currency_conversions,
+currency_conversions)
 
 
 
@@ -29,5 +29,8 @@ trade = Trader.new(currency_one)#, currency_conversions,
 # p currency_conversions.convert(currency_one, :USD)
 # p currency_conversions.convert(currency_one, :EUR)
 # p currency_conversions.convert(currency_three, :USD)
-# p currency_conversions.convert(currency_two, :JPY)
-p trade.initial_currency
+# p currency_conversions.convert(currency_two, :USD)
+# p trade.initial_rate.conversion_rates[:EUR]
+# p trade.money_trader
+p trade.usd_to_eur_and_back
+p trade.usd_to_jpy_and_back
--- currency_trader_class.rb
+++ currency_trader_class.rb
@@ -1,13 +1,35 @@
 class Trader
-  attr_reader :initial_currency#, :initial_rate, :second_rate
-  def initialize(initial_currency)#, initial_rate, second_rate)
-   @initial_currency = initial_currency
-  #  @amount = amount
-  #  @initial_rate = initial_rate
-  #  @second_rate = second_rate
+  attr_reader :initial_currency, :initial_rate, :second_rate
+  def initialize(initial_currency, initial_rate, second_rate)
+    @initial_currency = initial_currency
+    @initial_rate = initial_rate
+    @second_rate = second_rate
   end
 
-  # def code
-  #   @code = starting_currenty.name + start_currency.amount
-  # end
+#take initial_currency(in USD), convert it to EUR,
+#then converit it back to USD (assume time has passed
+#and the conversion rates have changed)
+  def money_trader
+  puts "You started with: " + "#{initial_currency.name} " +
+  "#{initial_currency.amount}"
+  puts "initial changes is : "# + "#{usd_to_eur_and_back}"
+  puts "the change back gives you: "
+  end
+
+  def usd_to_eur_and_back
+    usd_to_eur = initial_currency.amount * initial_rate.conversion_rates[:EUR] /
+    initial_rate.conversion_rates[initial_currency.name]
+    eur_back_to_usd = usd_to_eur * second_rate.conversion_rates[initial_currency.name] /
+    second_rate.conversion_rates[:EUR]
+  end
+
+  def usd_to_jpy_and_back
+    usd_to_jpy = initial_currency.amount * initial_rate.conversion_rates[:JPY] /
+    initial_rate.conversion_rates[initial_currency.name]
+    jpy_back_to_usd = usd_to_jpy * second_rate.conversion_rates[initial_currency.name] /
+    second_rate.conversion_rates[:JPY]
+    # p usd_to_jpy
+  end
+
+
 end
